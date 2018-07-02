45c84c837627a8bb2ab2074bd31ee113bd668034
Nelson <nelson@spirit-20.local>
2018-07-02 10:41:18 -0600

    fixed a few compiler errors

--- src/data.h
+++ src/data.h
@@ -3,12 +3,14 @@
 
 //#include <cmath>
 #include <fstream>
+#include <string>
 
 class TimeSeries {
+private:
     std::vector<float> rows;
     int numberOfAnts;
     int width;
-
+public:
     TimeSeries(int numAnts, int W);
     ~TimeSeries();
     //void nextLine(float* points);
